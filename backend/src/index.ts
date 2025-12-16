8f6f5b476576ce72fbcb3884dbf2be3fc8b34068
25LP2555 25LP2555 <25LP2555@pinecones-iMac.local>
2025-12-16 13:34:49 +0800

    need to fix this postman test whataver is happening

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,18 @@ import express from 'express';
 const app = express();
 const port = 4049;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json()); 
 
+const arr: string[] = [];
+
+app.get('/', (req, res) => {
+  res.json(arr)
+})
+app.post('/', (req, res) => {
+  const data = req.body
+  console.log("data: ", data)
+  res.send('success')
+})
 app.listen(port, () => {
   console.log(`Example app listening on port 4049`)
 })
