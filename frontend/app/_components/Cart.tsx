e6003cda4838a49d539d75e23f2200ba32954419
anueban049 <anudari.enkhbat@049@gmail.com>
2025-12-30 09:01:15 +0800

    merry christmass

--- frontend/app/_components/Cart.tsx
+++ frontend/app/_components/Cart.tsx
@@ -1,17 +1,18 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { ShoppingCart } from "lucide-react";
-import { useSidebar, SidebarProvider } from "@/components/ui/sidebar";
-import { UserSidebar } from "./sidebar";
-
-
+import { useSidebar} from "@/components/ui/sidebar";
 
 export function Cart() {
-
+ const {toggleSidebar} = useSidebar();
   return (
+   
     <>
- 
-        <Button size="icon" className="rounded-full bg-white">
+        <Button 
+        size="icon" 
+        className="rounded-full bg-white"
+        onClick={toggleSidebar}
+        >
           <ShoppingCart color="black" size={20} />
         </Button>
 
