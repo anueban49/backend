7706b7acbc45304ff4e35c4b0650cc97c9db9dee
anueban49 <anudari.enkhbat049@gmail.com>
2026-01-22 12:47:01 +0800

    wiuefui

--- frontend/app/_components/AuthProvider.tsx
+++ frontend/app/_components/AuthProvider.tsx
@@ -7,14 +7,29 @@ import {
 } from "react";
 import { formSchema } from "./userSchema";
 import type { userFormdata } from "./userSchema";
+import { api } from "@/lib/axios";
+import Router, { useRouter } from "next/router";
+
+type UserType = {
+  username: string;
+  email: string;
+  password: string;
+};
+type UserCompleteInfoType = {
+  username: string;
+  email: string;
+  _id: string;
+  timestamps: string;
+}; //this type is for response data
 export type AuthContextType = {
-  user: userFormdata | null;
+  user: UserType | null;
   error: Partial<Record<keyof userFormdata, string[]>> | null;
-  signup: (data: userFormdata) => void;
+  signup: (data: UserType) => Promise<void>;
+  login: ({ email, password }: UserType) => Promise<void>;
   logout: () => void;
 };
 export const AuthContext = createContext<AuthContextType | undefined>(
-  undefined
+  undefined,
 );
 export type AuthProviderProps = {
   children: ReactNode;
@@ -29,43 +44,35 @@ export function useAuth() {
   return context;
 } //-> this a custom hook. AI recommended and said it must be.
 export function AuthProvider({ children }: AuthProviderProps) {
-  const [user, setUser] = useState(() => {
-    const stored = sessionStorage.getItem("user");
-    if (!stored) return null;
-    const parsed = JSON.parse(stored);
-    const result = formSchema.safeParse(parsed);
-    return result.success ? result.data : null;
-  });
-
-  useEffect(() => {
-    if (user) {
-      sessionStorage.setItem("user:", JSON.stringify(user));
-    } else {
-      sessionStorage.removeItem("user");
-    }
-  }, [user]);
-
+  const [user, setUser] = useState<UserType | UserCompleteInfoType | null>(null);
   const [error, setError] = useState<Partial<
     Record<keyof userFormdata, string[]>
   > | null>(null);
+  const router = useRouter();
 
-  function signup(data: userFormdata) {
-    const result = formSchema.safeParse(data);
-    if (!result.success) {
-      setError(result.error.flatten().fieldErrors);
-      return;
+  const signup = async (data: UserType) => {
+    try {
+      await api.post<UserType>("/user/register", data);
+      console.log("new user created:");
+      setUser((data) => data)
+    } catch (error) {
+      console.error(error);
     }
-
-    setError(null);
-    setUser(result.data);
-  }
-
+  };
+  const login = async ({ email, password }: UserType) => {
+    try {
+      await api.post("user/login", { email, password });
+    } catch (error) {
+      console.error(error);
+    }
+    router.push("/home");
+  };
   function logout() {
     setUser(null);
   }
 
   return (
-    <AuthContext.Provider value={{ user, error, signup, logout }}>
+    <AuthContext.Provider value={{ user, error, signup, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
